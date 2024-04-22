a704b2f720f8fb5f84d88c5ddba56d8a4685cd98
Joaquim Gomes <joaquim_og@hotmail.com>
2024-04-22 17:23:29 -0300

    added voice to text ios viewModel

---
+++ iosApp/iosApp/voice_to_text/presentation/IOSVoiceToTextViewModel.swift
@@ -0,0 +1,45 @@
+import Foundation
+import shared
+import Combine
+
+@MainActor class IOSVoiceToTextViewModel: ObservableObject {
+    private var parser: any VoiceToTextParser
+    private let languageCode: String
+    
+    private let viewModel: VoiceToTextViewModel
+    @Published var state = VoiceToTextState(
+        powerRatios: [],
+        spokenText: "",
+        canRecord: false,
+        recordError: nil,
+        displayState: nil
+    )
+    
+    private var handle: DisposableHandle?
+    
+    init(parser: VoiceToTextParser, langagueCode: String) {
+        self.parser = parser
+        self.languageCode = langagueCode
+        self.viewModel = VoiceToTextViewModel(
+        parser: parser,
+        coroutineScope: nil
+        )
+    }
+    
+    func onEvent(event: VoiceToTextEvent) {
+        viewModel.onEvent(event: event)
+    }
+    
+    func startObserving() {
+        handle = viewModel.state.subscribe { [weak self] state in
+            if let state {
+                self?.state = state
+            }
+        }
+    }
+    
+    func dispose() {
+        handle?.dispose()
+        onEvent(event: VoiceToTextEvent.Reset())
+    }
+}
